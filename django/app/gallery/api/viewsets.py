7b86ad3fd64379585cd89a0c8e0290c322de679b
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2021-01-20 01:46:04 -0300

    Add filtering and ordering into Photos and Albums REST API

--- django/app/gallery/api/viewsets.py
+++ django/app/gallery/api/viewsets.py
@@ -1,17 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from gallery.models import Album, Photo
 from .serializers import AlbumSerializer, PhotoSerializer
 
 class AlbumViewSet(ModelViewSet):
-    queryset = Album.objects.order_by('-id').all()
+    queryset = Album.objects.all()
     serializer_class = AlbumSerializer
     permission_classes = (IsAuthenticated,)
     authentication_classes = (TokenAuthentication,)
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    filterset_fields = ['id', 'created', 'updated', 'name', 'description']
+    ordering_fields = ['id', 'created', 'updated', 'name']
+    ordering = ['id']
 
 class PhotoViewSet(ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
     permission_classes = (IsAuthenticated,)
     authentication_classes = (TokenAuthentication,)
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    filterset_fields = ['id', 'album_fk', 'created', 'updated', 'name'] 
+    ordering_fields = ['id', 'album_fk', 'created', 'updated', 'name']
+    ordering = ['id']
--- django/app/lwandisurf/settings.py
+++ django/app/lwandisurf/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_summernote',
+    'django_filters',
     # apps
     'core',
     'gallery',
