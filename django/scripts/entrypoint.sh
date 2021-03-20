e3188e83b238e62bb8b77d6d9c0ea01ff853c72b
Cassiano Andrei <cschneider@inf.ufsm.br>
2021-03-20 07:03:33 -0300

    Ajustes nginx Docker

--- django/scripts/entrypoint.sh
+++ django/scripts/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
@@ -10,21 +8,11 @@ then
       sleep 0.1
     done
 
-    echo "PostgreSQL started!"
+    echo "PostgreSQL started"
 fi
 
-if [ "$HTTP_SERVER" = "gunicorn" ]
-then
-    echo "Running Gunicorn HTTP server..."
-    gunicorn $PROJECT_NAME.wsgi:application --bind 0.0.0.0:8000
-
-elif [ "$HTTP_SERVER" = "django" ]
-then
-    echo "Running standard Django HTTP server..."
-    python manage.py runserver 0.0.0.0:8000
-    
-else
-    echo "Invalid HTTP Server!"
-fi
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --no-input
 
 exec "$@"
